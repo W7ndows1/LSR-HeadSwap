7245e6e81a63fbd35aa9d5a0d56e10ad4b7960c2
W7ndows1 <148021554+W7ndows1@users.noreply.github.com>
2026-02-15 22:37:08 -0800

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,31 @@
-package com.example;
+package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    @Override
+    public void onInitialize() {
+        UseItemCallback.EVENT.register((player, world, hand) -> {
+            ItemStack held = player.getStackInHand(hand);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+            if (!held.isOf(Items.PLAYER_HEAD)) return ActionResult.PASS;
+            if (world.isClient()) return ActionResult.SUCCESS;
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+            ItemStack helmet = player.getEquippedStack(EquipmentSlot.HEAD);
+            ItemStack head = held.copy();
+            head.setCount(1);
 
-		LOGGER.info("Hello Fabric world!");
-	}
+            player.equipStack(EquipmentSlot.HEAD, head);
+
+            if (helmet.isEmpty()) held.decrement(1);
+            else player.setStackInHand(hand, helmet);
+
+            return ActionResult.SUCCESS;
+        });
+    }
 }
